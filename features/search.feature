3e21a3c78d1d27bcfbbb3f025353048b28e8073f
robitalhazmi <robitalhazmi@gmail.com>
2018-11-30 21:50:00 +0700

    add people search

--- features/search.feature
+++ features/search.feature
@@ -1,7 +1,11 @@
 Feature: Search
 
-  Scenario: Search User
+  Scenario: Top Search
     Given I navigate to the Twitter Search page
     When I search for "Robit Alhazmi"
     Then I am taken to Twitter Search Results page
-    And I see a search result "Robit Alhazmi"
+
+  Scenario: Search User
+    Given Top search results for "Robit Alhazmi" are shown
+    When I click on the "People" link
+    Then User related to "Robit Alhazmi" are shown on the results page
--- features/steps/search_steps.py
+++ features/steps/search_steps.py
@@ -1,6 +1,6 @@
 from nose.tools import assert_equal, assert_true
 from selenium.webdriver.common.by import By
-
+# Top Search Scenario
 @step('I navigate to the Twitter Search page')
 def step_impl(context):
     context.search_page.navigate("https://twitter.com/search-home")
@@ -13,7 +13,17 @@ def step_impl(context, search_term):
 @step('I am taken to Twitter Search Results page')
 def step_impl(context):
     assert_equal(context.search_results_page.get_page_title(), "Twitter Search")
+# Search User Scenario
 
-@step('I see a search result "{search_result}"')
+@step('Top search results for "{search_term}" are shown')
+def step_impl(context, search_term):
+    context.search_page.navigate("https://twitter.com/search?src=typd&q=Robit%20Alhazmi")
+    assert_equal(context.search_page.get_page_title(), "Robit Alhazmi - Twitter Search")
+
+@step('I click on the "{search_term}" link')
+def step_impl(context, search_term):
+    context.search_page.click_element(By.LINK_TEXT, search_term)
+
+@step('User related to "{search_result}" are shown on the results page')
 def step_impl(context, search_result):
     assert_true(context.search_results_page.find_search_result(search_result))
